67e4144a3b8f99bda794d1e4ebec019a02831787
Jonathan Bergqvist <jonathan@seednode.io>
2022-09-03 17:23:48 +0200

    Add script for listing EC2 instances

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ key-pair:
 	python create_key_pair.py && chmod 400 ec2-key-pair.pem
 ec2-create:
 	python create_ec2_instance.py
+ec2-list:
+	python list_ec2_instances.py
+# ec2-connect:
---
+++ list_ec2_instances.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+import boto3
+
+"""
+A tool for retrieving basic information from the running EC2 instances.
+"""
+
+# Connect to EC2
+ec2 = boto3.resource("ec2")
+
+# Get information for all running instances
+running_instances = ec2.instances.filter(
+    Filters=[{"Name": "instance-state-name", "Values": ["running"]}]
+)
+
+if len(list(running_instances)) == 0:
+    print("No instances active.")
+
+ec2info = defaultdict()
+for instance in running_instances:
+    name = "-"
+
+    if instance.tags is not None:
+        for tag in instance.tags:
+            if tag["Key"] == "Name":
+                name = tag["Value"]
+
+    # Add instance info to a dictionary
+    instance_info = {
+        "Name": name,
+        "Type": instance.instance_type,
+        "State": instance.state["Name"],
+        "Private IP": instance.private_ip_address,
+        "Public IP": instance.public_ip_address,
+        "Launch Time": instance.launch_time,
+        "Public DNS Name": instance.public_dns_name,
+    }
+
+    ec2info[instance.id] = instance_info
+
+    for attr, instance_value in instance_info.items():
+        print("{0}: {1}".format(attr, instance_value))
+
+    print(
+        f"\nConnect command: \nssh -i ec2-key-pair.pem ubuntu@{instance.public_dns_name}"
+    )
+    print("------")
